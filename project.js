450bee5f1c1db5a9df8b91577c68b99d80871b4f
auwalisa36 <72025080+auwalisa36@users.noreply.github.com>
2020-11-19 09:29:23 +0100

    Add files via upload

---
+++ project.js
@@ -0,0 +1,8 @@
+function submitinfo(){
+    if (yourName =='' || emailAddress =='' || yourMessage =='') {
+        alert (' field the missing space')
+    }
+    else{
+        alert('form has been submitted' )
+    }
+}
